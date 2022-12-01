f2a485b2d59cc67b526d8fe1bb919cdc3d3c9955
Luisa Velasquez <luvlasquez96@gmail.com>
2022-12-01 15:21:55 -0500

    feature: saving method

--- app/src/main/java/com/example/drawyourself/MainActivity.kt
+++ app/src/main/java/com/example/drawyourself/MainActivity.kt
@@ -4,9 +4,14 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -16,7 +21,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -87,6 +101,16 @@ class MainActivity : AppCompatActivity() {
         undoButton.setOnClickListener {
             drawingView?.onClickUndo()
         }
+        val saveButton: ImageButton = findViewById(R.id.ibSave)
+        undoButton.setOnClickListener {
+            if(isReadStorageAllowed()){
+                lifecycleScope.launch{
+                    val flDrawingView: FrameLayout=findViewById(R.id.fl_view)
+                    //val myBitmap: Bitmap=getBitmapFromView(flDrawingView)
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
     }
 
     private fun showSizeBrushDialog() {
@@ -110,6 +134,11 @@ class MainActivity : AppCompatActivity() {
         }
         brushDialog.show()
     }
+    private fun isReadStorageAllowed():Boolean{
+        val result=ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result== PackageManager.PERMISSION_GRANTED
+    }
 
     private fun requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
@@ -119,9 +148,9 @@ class MainActivity : AppCompatActivity() {
         ) {
             showRationaleDialog("DrawYourSelf", "Needs to Access Your External Storage")
         } else {
-            requestPermission.launch(
-                arrayOf(
-                    Manifest.permission.READ_EXTERNAL_STORAGE
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
                 )
             )
         }
@@ -155,5 +184,55 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnBitmap = Bitmap.createBitmap(
+            view.width, view.height,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(returnBitmap)
+        val bgDrawable = view.background
+
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+
+        return returnBitmap
+
+    }
+    private suspend fun saveBitmapFile(mBitmap:Bitmap?):String{
+        var result=""
+        withContext(Dispatchers.IO){
+            if (mBitmap!= null){
+                try {
+                    val bytes= ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val file= File (externalCacheDir?.absoluteFile.toString()
+                            + File.separator + "Drawyourself_" + System.currentTimeMillis()/1000 + ".png")
+                    val fo= FileOutputStream(file)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    result= file.absolutePath
+
+                    runOnUiThread{
+                        if(result.isNotEmpty()){
+                            Toast.makeText(this@MainActivity,"File saved successfully:$result",
+                                Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this@MainActivity, "Wrong saving file", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }catch (e:Exception){
+                    result=""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 }
